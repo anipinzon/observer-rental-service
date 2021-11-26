9356c803f257227586cd8f8136ba07210d9a58ff
Nicolás Pinzón <apinzo1@bancodebogota.com.co>
2021-11-25 20:58:50 -0500

    fix compose and string connection

--- src/controllers/MongoDB.ts
+++ src/controllers/MongoDB.ts
@@ -1,12 +1,14 @@
 import { Observer } from "./interfaces";
 const db = require("mongoose");
 
+let MONGODB_STRING_CONECTION = process.env.MONGODB_STRING_CONECTION || "mongodb://root:example@mongodb:27017/admin"
+
 export class MongoDB implements Observer {
   public update(id: string): void {
     console.log(`SOY MONGODB Y TENGO EL ID ${id}`);
     db.Promise = global.Promise;
     db.connect(
-      `mongodb://${process.env.MONGO_USUARIO}:${process.env.MONGO_CONTRASEÑA}@${process.env.MONGO_HOST}/${process.env.MONGO_BD}`
+      MONGODB_STRING_CONECTION
     );
     
   }
